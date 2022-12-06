3e6ded39e36ff5be862f8e056394ae6db743f192
ra <rawia.ben.amira1997@gmail.com>
2022-12-06 22:59:54 +0100

    init classes module+salle

--- src/main/java/fr/univ/tln/projets/projetJava/EDT/Classes/Module.java
+++ src/main/java/fr/univ/tln/projets/projetJava/EDT/Classes/Module.java
@@ -1,4 +1,40 @@
 package fr.univ.tln.projets.projetJava.EDT.Classes;
 
 public class Module {
+    private String codeMod ;
+    private String libelleMod ;
+    private int volumeHoraire ;
+
+    private Module(String codeMod, String libelleMod, int volumeHoraire){
+        this.codeMod = codeMod ;
+        this.libelleMod = libelleMod ;
+        this.volumeHoraire = volumeHoraire ;
+    }
+    public static Module of(String codeMod, String libelleMod, int volumeHoraire){
+        return new Module(codeMod, libelleMod, volumeHoraire);
+    }
+
+    public void setCodeMod(String codeMod) {
+        this.codeMod = codeMod;
+    }
+
+    public void setLibelleMod(String libelleMod) {
+        this.libelleMod = libelleMod;
+    }
+
+    public void setVolumeHoraire(int volumeHoraire) {
+        this.volumeHoraire = volumeHoraire;
+    }
+
+    public int getVolumeHoraire() {
+        return volumeHoraire;
+    }
+
+    public String getCodeMod() {
+        return codeMod;
+    }
+
+    public String getLibelleMod() {
+        return libelleMod;
+    }
 }
