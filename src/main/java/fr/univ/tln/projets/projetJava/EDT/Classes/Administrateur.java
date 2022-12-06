e83f73332330470e0e153bc5774bebf7c4c6e273
ra <rawia.ben.amira1997@gmail.com>
2022-12-06 22:36:16 +0100

    init classes utilisateur

--- src/main/java/fr/univ/tln/projets/projetJava/EDT/Classes/Administrateur.java
+++ src/main/java/fr/univ/tln/projets/projetJava/EDT/Classes/Administrateur.java
@@ -1,4 +1,10 @@
 package fr.univ.tln.projets.projetJava.EDT.Classes;
 
 public class Administrateur extends Utilisateur{
+    private Administrateur(int id, String nom, String prenom, String email, int tel, String login, String mdp){
+        super(id, nom, prenom, email, tel, login, mdp);
+    }
+    public static Administrateur of(int id, String nom, String prenom, String email, int tel, String login, String mdp){
+        return new Administrateur(id, nom, prenom, email, tel, login, mdp);
+    }
 }
--- src/main/java/fr/univ/tln/projets/projetJava/EDT/Classes/Enseignant.java
+++ src/main/java/fr/univ/tln/projets/projetJava/EDT/Classes/Enseignant.java
@@ -1,4 +1,22 @@
 package fr.univ.tln.projets.projetJava.EDT.Classes;
 
 public class Enseignant extends Utilisateur{
+    private String grade ;
+    private Enseignant(int id, String nom, String prenom, String email, int tel, String login, String mdp, String grade)
+    {
+        super(id, nom, prenom, email, tel, login, mdp);
+        this.grade = grade ;
+    }
+    public static Enseignant of(int id, String nom, String prenom, String email, int tel, String login, String mdp, String grade){
+        return new Enseignant(id, nom, prenom, email, tel, login, mdp, grade);
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
 }
+
