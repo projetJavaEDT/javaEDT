7fd24070bbde01b367f0dd17374efdf5c26a5de4
meryam <meryambenhammane532@gmailcom>
2022-12-09 09:09:01 +0100

    add seance ..

--- src/main/java/fr/univ/tln/projets/projetJava/EDT/Classes/Indisponibilite.java
+++ src/main/java/fr/univ/tln/projets/projetJava/EDT/Classes/Indisponibilite.java
@@ -1,4 +1,62 @@
 package fr.univ.tln.projets.projetJava.EDT.Classes;
 
+import java.util.*;
+
 public class Indisponibilite {
+    private int id;
+    private boolean indisponibilite;
+    private Date jour;
+
+
+    public Indisponibilite(int id, boolean indisponibilite, Date jour) {
+        this.id = id;
+        this.indisponibilite = indisponibilite;
+        this.jour = jour;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isIndisponibilite() {
+        return indisponibilite;
+    }
+
+    public void setIndisponibilite(boolean indisponibilite) {
+        this.indisponibilite = indisponibilite;
+    }
+
+    public Date getJour() {
+        return jour;
+    }
+
+    public void setJour(Date jour) {
+        this.jour = jour;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Indisponibilite that = (Indisponibilite) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Indisponibilite{" +
+                "id=" + id +
+                ", indisponibilite=" + indisponibilite +
+                ", jour=" + jour +
+                '}';
+    }
 }
--- src/main/java/fr/univ/tln/projets/projetJava/EDT/Classes/Seance.java
+++ src/main/java/fr/univ/tln/projets/projetJava/EDT/Classes/Seance.java
@@ -1,6 +1,86 @@
 package fr.univ.tln.projets.projetJava.EDT.Classes;
 
 
+import java.util.*;
+
 public class Seance {
 
+    private int id;
+    private String type;
+    private Date jour;
+    private int heureDebut;
+    private int heureFin;
+
+    public Seance(int id, String type, Date jour, int heureDebut, int heureFin) {
+        this.id = id;
+        this.type = type;
+        this.jour = jour;
+        this.heureDebut = heureDebut;
+        this.heureFin = heureFin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getJour() {
+        return jour;
+    }
+
+    public void setJour(Date jour) {
+        this.jour = jour;
+    }
+
+    public int getHeureDebut() {
+        return heureDebut;
+    }
+
+    public void setHeureDebut(int heureDebut) {
+        this.heureDebut = heureDebut;
+    }
+
+    public int getHeureFin() {
+        return heureFin;
+    }
+
+    public void setHeureFin(int heureFin) {
+        this.heureFin = heureFin;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Seance seance = (Seance) o;
+        return id == seance.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Seance{" +
+                "id=" + id +
+                ", type='" + type + '\'' +
+                ", jour=" + jour +
+                ", heureDebut=" + heureDebut +
+                ", heureFin=" + heureFin +
+                '}';
+    }
 }
