5ca80d04ad05fd695762b53de7ccf8bb88ed63c0
rawi124 <rawia.ben.amira1997@gmail.com>
2022-12-14 14:15:33 +0100

    seanceBuilder

--- src/main/java/fr/univ/tln/projets/projetJava/EDT/Classes/Seance.java
+++ src/main/java/fr/univ/tln/projets/projetJava/EDT/Classes/Seance.java
@@ -3,84 +3,123 @@ package fr.univ.tln.projets.projetJava.EDT.Classes;
 
 import java.util.*;
 
+import java.util.Date;
+
 public class Seance {
+    public enum Type {
+        TP, TD, CM;
+    }
+    private String codeMod ;
+    private String codeSalle;
+    private Type typeSeance;
+    private Date date;
+    private String heureD;
+    private String heureF;
 
-    private int id;
-    private String type;
-    private Date jour;
-    private int heureDebut;
-    private int heureFin;
 
-    public Seance(int id, String type, Date jour, int heureDebut, int heureFin) {
-        this.id = id;
-        this.type = type;
-        this.jour = jour;
-        this.heureDebut = heureDebut;
-        this.heureFin = heureFin;
+
+    public static class SeanceBuilder{
+        private String codeMod ;
+        private String codeSalle;
+        private Type typeSeance;
+        private Date date;
+        private String heureD;
+        private String heureF;
+
+
+        public SeanceBuilder codeMod(String codeMod) {
+            this.codeMod = codeMod;
+            return this;
+        }
+        public SeanceBuilder codeSalle(String codeSalle) {
+            this.codeSalle = codeSalle;
+            return this;
+        }
+        public SeanceBuilder typeSeance(Type typeSeance) {
+            this.typeSeance = typeSeance;
+            return this;
+        }
+        public SeanceBuilder date(Date date) {
+            this.date = date;
+            return this;
+        }
+        public SeanceBuilder heureD(String heureD) {
+            this.heureD = heureD;
+            return this;
+        }
+        public SeanceBuilder heureF(String heureF) {
+            this.heureF = heureF;
+            return this;
+        }
+
+
+        public Seance build() {
+            Seance seance = new Seance();
+            seance.codeMod = codeSalle;
+            seance.codeSalle = codeSalle;
+            seance.heureD = heureD;
+            seance.heureF = heureF;
+            seance.date = date;
+            return seance;
+        }
+    }
+    public String getCodeMod() {
+        return codeMod;
     }
 
-    public int getId() {
-        return id;
+    public void setCodeMod(String codeMod) {
+        this.codeMod = codeMod;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public String getCodeSalle() {
+        return codeSalle;
     }
 
-    public String getType() {
-        return type;
+    public void setCodeSalle(String codeSalle) {
+        this.codeSalle = codeSalle;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public Type getTypeSeance() {
+        return typeSeance;
     }
 
-    public Date getJour() {
-        return jour;
+    public void setTypeSeance(Type typeSeance) {
+        this.typeSeance = typeSeance;
     }
 
-    public void setJour(Date jour) {
-        this.jour = jour;
+    public Date getDate() {
+        return date;
     }
 
-    public int getHeureDebut() {
-        return heureDebut;
+    public void setDate(Date date) {
+        this.date = date;
     }
 
-    public void setHeureDebut(int heureDebut) {
-        this.heureDebut = heureDebut;
+    public String getHeureD() {
+        return heureD;
     }
 
-    public int getHeureFin() {
-        return heureFin;
+    public void setHeureD(String heureD) {
+        this.heureD = heureD;
     }
 
-    public void setHeureFin(int heureFin) {
-        this.heureFin = heureFin;
+    public String getHeureF() {
+        return heureF;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Seance seance = (Seance) o;
-        return id == seance.id;
+    public void setHeureF(String heureF) {
+        this.heureF = heureF;
     }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
 
     @Override
     public String toString() {
         return "Seance{" +
-                "id=" + id +
-                ", type='" + type + '\'' +
-                ", jour=" + jour +
-                ", heureDebut=" + heureDebut +
-                ", heureFin=" + heureFin +
+                "codeMod='" + codeMod + '\'' +
+                ", date=" + date +
+                ", heureD='" + heureD + '\'' +
+                ", heureF='" + heureF + '\'' +
                 '}';
     }
+
+
 }
