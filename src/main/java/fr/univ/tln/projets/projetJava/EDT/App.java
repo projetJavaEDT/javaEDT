4286c356bea8df0994ab3083c514189ff1afddd6
rawi124 <rawia.ben.amira1997@gmail.com>
2022-12-17 00:13:18 +0100

    add entete exceptions

--- src/main/java/fr/univ/tln/projets/projetJava/EDT/App.java
+++ src/main/java/fr/univ/tln/projets/projetJava/EDT/App.java
@@ -10,18 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        String str = "prof";
-        String email = "rawiabenamira@gmail.fr";
-        Security sec = new Security(str);
-
-
-
-        String x ="9bc2e1d2b5bcdababaedf20ddf58f004c495b235cbf2d18bcbbc528d2b67fc4d";
-        String y = sec.hacherMdp(str);
-        System.out.println(y);
-
-
-
         EDTApplication.main(args);
     }
 }
---
+++ src/main/java/fr/univ/tln/projets/projetJava/EDT/Exceptions/ExceptionNbMaxPlacesSalle.java
@@ -0,0 +1,7 @@
+package fr.univ.tln.projets.projetJava.EDT.Exceptions;
+
+public class ExceptionNbMaxPlacesSalle extends Exception {
+    public ExceptionNbMaxPlacesSalle() {
+        super("Nb max etudiants depassé ! ");
+    }
+}
